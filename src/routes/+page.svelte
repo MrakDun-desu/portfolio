c02acb6c26f39de3fba05ee6b656a1bf71f0c363
Marek Danco <m.danco42@gmail.com>
2026-09-13 21:19:21 +0200

    Add basic info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	const age = new Date(Date.now()).getUTCFullYear() - 2000;
+</script>
+
+<h1>mrak.click</h1>
+
+<p>☁️ hello, i'm mrak! ☁️</p>
+
+<p>i'm a full-stack developer, specializing in C# and TypeScript. I'm also:</p>
+
+<ul>
+	<li>{age} years old</li>
+	<li>the founder and leader of Czech/Slovak Tetris community</li>
+	<li>genderfluid (he/she/they)</li>
+	<li>former boy scout - i like camping, hiking, and nature!</li>
+	<li>using <code>CachyOS</code> btw</li>
+	<li>occasional cosplayer (usually with my girlfriend)</li>
+</ul>
