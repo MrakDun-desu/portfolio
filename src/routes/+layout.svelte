7d7e31c976d36a3f2744e24fae8c313821b27a1c
Marek Danco <m.danco42@gmail.com>
2026-09-14 20:56:54 +0200

    Test fantasque sans font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.png';
+	import "./style.css"
 
 	let { children } = $props();
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-	const age = new Date(Date.now()).getUTCFullYear() - 2000;
+	const date = new Date(Date.now());
+	const over_birthday = date.getMonth() > 4 || date.getDay() > 16;
+	const age = over_birthday ? date.getFullYear() - 2000 : date.getFullYear() - 2001;
 </script>
 
 <h1>mrak.click</h1>
@@ -10,9 +12,17 @@
 
 <ul>
 	<li>{age} years old</li>
-	<li>the founder and leader of Czech/Slovak Tetris community</li>
+	<li>the founder and owner of Czech/Slovak Tetris community</li>
 	<li>genderfluid (he/she/they)</li>
 	<li>former boy scout - i like camping, hiking, and nature!</li>
 	<li>using <code>CachyOS</code> btw</li>
 	<li>occasional cosplayer (usually with my girlfriend)</li>
 </ul>
+
+<h2>Projects</h2>
+
+<h3><a href="https://github.com/MrakDun-desu/UStacker-old" target="_blank">UStacker</a></h3>
+<p>A super-customizable block stacking game made in Unity</p>
+<p><i>2022 - 2024 (discontinued)</i></p>
+
+<p>...more incoming</p>
